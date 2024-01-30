4edd867f30d42d9a26ad44d16d0518eb98004853
Ashu Negi <anegi6723@gmail.com>
2024-01-30 06:04:21 +0530

    adding contact schema

---
+++ models/contactModel.js
@@ -0,0 +1,24 @@
+const { Timestamp } = require("mongodb");
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : [true,"Please Enter contact name"]
+    },
+    email : {
+        type : String,
+        required : [true,"Please Enter contact email address"]
+    },
+    phone : {
+        type : String,
+        required : [true,"Please Enter contact phone number"]
+    },
+},
+{
+    timestamps : true, 
+    // This option, when set to true in a Mongoose schema, automatically adds two fields to your documents: createdAt and updatedAt. These fields store timestamps representing when the document was created and when it was last modified, respectively.
+}
+)
+
+module.exports = mongoose.model("Contact",contactSchema)
