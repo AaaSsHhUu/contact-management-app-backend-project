c1fb67cd64b620335d0fe2ae3df11924d4f41356
Ashu Negi <anegi6723@gmail.com>
2024-02-01 13:48:25 +0530

    initializing routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router();
+
+router.post("/register", (req,res)=>{
+    res.json({message : "Reister the user"});
+})
+router.post("/login", (req,res)=>{
+    res.json({message : "Login user"});
+})
+router.post("/current", (req,res)=>{
+    res.json({message : "Current user information"});
+})
+
+module.exports = router;
