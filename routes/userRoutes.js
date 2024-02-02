42857850daa50fd44e1362eb727e721bb7b28adf
Ashu Negi <anegi6723@gmail.com>
2024-02-02 19:15:08 +0530

    added userController functions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express")
 const router = express.Router();
+const {registerUser,loginUser,currentUser} = require("../Controllers/userController");
 
-router.post("/register", (req,res)=>{
-    res.json({message : "Reister the user"});
-})
-router.post("/login", (req,res)=>{
-    res.json({message : "Login user"});
-})
-router.get("/current", (req,res)=>{
-    res.json({message : "Current user information"});
-})
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/current", currentUser);
 
 module.exports = router;
