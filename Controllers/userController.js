f28d82f5ab3658875750f3a9639bac3d88899a54
Ashu Negi <anegi6723@gmail.com>
2024-02-02 20:10:21 +0530

    added registering feature

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,10 +1,25 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 // @desc  register
 // @route POST /api/users/register
 // @access public
 const registerUser = asyncHandler(async (req,res)=>{
-    res.json({message : "Reister the user"});
+    let {username, email, password} = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error(`User with email : ${email} already registered`);
+    }
+
+    let hashedPassword = await bcrypt.hash(password,10); // bcrypt.hash(<plaintext>,<salt_rounds>)
+    console.log(hashedPassword);
+    res.json({message : "registered"});
 })
 
 // @desc login
