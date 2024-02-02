90453507b0db81e4ff991b69fe135535946c37f8
Ashu Negi <anegi6723@gmail.com>
2024-02-02 19:14:28 +0530

    updated documentation

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,21 +1,21 @@
 const asyncHandler = require("express-async-handler");
 
-// @desc of register
+// @desc  register
 // @route POST /api/users/register
 // @access public
 const registerUser = asyncHandler(async (req,res)=>{
     res.json({message : "Reister the user"});
 })
 
-// @desc of register
-// @route POST /api/users/register
+// @desc login
+// @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req,res)=>{
     res.json({message : "Login user"});
 })
 
-// @desc of register
-// @route POST /api/users/register
+// @desc current user
+// @route GET /api/users/current
 // @access public
 const currentUser = asyncHandler(async (req,res)=>{
     res.json({message : "Current user information"});
