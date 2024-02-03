aa38a14eaae64826422b1c578d9749aa6e51726a
Ashu Negi <anegi6723@gmail.com>
2024-02-03 11:16:57 +0530

    adding validateToken middleware in current user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
 const {registerUser,loginUser,currentUser} = require("../Controllers/userController");
+const validateToken = require("../middleware/ValidateToken");
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", currentUser);
+router.get("/current",validateToken, currentUser);
 
 module.exports = router;
