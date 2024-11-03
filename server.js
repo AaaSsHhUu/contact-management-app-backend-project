28f99f5ef88d1312541dd621d8b90af53f20dd00
Ashu Negi <anegi6723@gmail.com>
2024-11-03 23:21:41 +0530

    used cookie-parser

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
+const cookieParser = require("cookie-parser");
 const connectdb = require("./config/dbConnection");
 
 const app = express();
@@ -9,6 +11,8 @@ connectdb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser()); //  The cookie-parser middleware only parses incoming cookies and makes them available on req.cookies (and req.signedCookies if signed).
+
 app.use("/api/contacts" , require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
@@ -16,3 +20,5 @@ app.use(errorHandler);
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
 })
+
+// TODO : create admin routes to get all users and manage them
